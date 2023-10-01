ac020f848531bfbb2564d920e506f91a1e0704b0
Lee Connelly <lee@leeconnelly.dev>
2023-10-01 01:45:53 +0100

    Use League Spartan font

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,7 +9,7 @@
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.bunny.net/css?family=league-spartan:500,700" rel="stylesheet" />
 
         <!-- Scripts -->
         @routes
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,19 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['League Spartan', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                'light-gray': '#F8F8FB',
+                'border-gray': '#DFE3FA',
+                'light-purple': '#7E88C3',
+                'purple': '#7C5DFA',
+                'black': '#0C0E16',
+                'blue': '#373B53',
+                'light-blue': '#7E88C3',
+                'red': '#EC5757',
+                'green': '#33D69F',
+                'orange': '#FF8F00',
             },
         },
     },
