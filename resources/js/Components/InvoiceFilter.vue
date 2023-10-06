f16b172876196f7cc76a2d7e25f1018c4a712d54
Lee Connelly <lee@leeconnelly.dev>
2023-10-06 22:41:28 +0100

    Remove unused variable

--- resources/js/Components/InvoiceFilter.vue
+++ resources/js/Components/InvoiceFilter.vue
@@ -10,8 +10,6 @@ defineProps({
     label: String,
 })
 
-const ignoreElement = ref(null)
-
 defineEmits(['update:modelValue'])
 </script>
 
@@ -24,7 +22,6 @@ defineEmits(['update:modelValue'])
         v-if="open"
         v-on-click-outside="() => (open = false)"
         class="absolute top-full right-0 bg-white rounded-md shadow-md pl-4 pr-6 pt-3 pb-3 mt-1 justify-items-start grid gap-1"
-        ref="ignoreElement"
     >
         <button @click="$emit('update:modelValue', 2)" type="button">
             Paid
