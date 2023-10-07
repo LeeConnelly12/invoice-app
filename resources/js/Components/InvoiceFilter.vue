946d7b404440ef117adde8e9f0ff23f47f117ebc
Lee Connelly <lee@leeconnelly.dev>
2023-10-07 19:55:16 +0100

    Cilcking filter closes dropdown

--- resources/js/Components/InvoiceFilter.vue
+++ resources/js/Components/InvoiceFilter.vue
@@ -11,16 +11,30 @@ defineProps({
 })
 
 defineEmits(['update:modelValue'])
+
+const ignoreElement = ref(null)
+
+const clickedOutside = [
+    () => {
+        open.value = false
+    },
+    { ignore: [ignoreElement] },
+]
 </script>
 
 <template>
-    <button @click="open = !open" type="button" class="flex items-center gap-2">
+    <button
+        @click="open = !open"
+        ref="ignoreElement"
+        type="button"
+        class="flex items-center gap-2"
+    >
         <p class="font-bold">{{ label }}</p>
         <Chevron class="transform" :class="{ 'rotate-180': open }" />
     </button>
     <div
         v-if="open"
-        v-on-click-outside="() => (open = false)"
+        v-on-click-outside="clickedOutside"
         class="absolute top-full right-0 bg-white rounded-md shadow-md pl-4 pr-6 pt-3 pb-3 mt-1 justify-items-start grid gap-1"
     >
         <button @click="$emit('update:modelValue', null)" type="button">
