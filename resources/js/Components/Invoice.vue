8364b9a806b795b8adfc98e78334507305e4dedd
Lee Connelly <lee@leeconnelly.dev>
2023-10-07 19:49:32 +0100

    Create InvoiceStatus component

--- resources/js/Components/Invoice.vue
+++ resources/js/Components/Invoice.vue
@@ -1,31 +1,12 @@
 <script setup>
 import { inject } from 'vue'
+import InvoiceStatus from '@/Components/InvoiceStatus.vue'
 
 defineProps({
     invoice: Object,
 })
 
 const emitter = inject('emitter')
-
-const statusText = (status) => {
-    const statuses = {
-        0: 'Draft',
-        1: 'Pending',
-        2: 'Paid',
-    }
-
-    return statuses[status]
-}
-
-const statusColor = (status) => {
-    const colors = {
-        0: 'bg-blue/10 text-blue',
-        1: 'bg-orange/10 text-orange',
-        2: 'bg-green/10 text-green',
-    }
-
-    return colors[status]
-}
 </script>
 
 <template>
@@ -46,13 +27,7 @@ const statusColor = (status) => {
         <div class="grid grid-cols-2 grid-rows-2 items-center pt-6">
             <p class="text-light-blue text-sm text-left">Due 19 Aug 2021</p>
             <div class="text-right row-span-2">
-                <div
-                    :class="statusColor(invoice.status)"
-                    class="w-28 h-10 inline-flex items-center justify-center gap-2 font-bold rounded-md"
-                >
-                    <div class="rounded-full w-2 h-2 bg-current"></div>
-                    {{ statusText(invoice.status) }}
-                </div>
+                <InvoiceStatus :status="invoice.status" />
             </div>
             <p class="font-bold text-left mt-1">$1,800.90</p>
         </div>
---
+++ resources/js/Components/InvoiceStatus.vue
@@ -0,0 +1,35 @@
+<script setup>
+defineProps({
+    status: Number,
+})
+
+const statusText = (status) => {
+    const statuses = {
+        0: 'Draft',
+        1: 'Pending',
+        2: 'Paid',
+    }
+
+    return statuses[status]
+}
+
+const statusColor = (status) => {
+    const colors = {
+        0: 'bg-blue/10 text-blue',
+        1: 'bg-orange/10 text-orange',
+        2: 'bg-green/10 text-green',
+    }
+
+    return colors[status]
+}
+</script>
+
+<template>
+    <div
+        :class="statusColor(status)"
+        class="w-28 h-10 inline-flex items-center justify-center gap-2 font-bold rounded-md"
+    >
+        <div class="rounded-full w-2 h-2 bg-current"></div>
+        {{ statusText(status) }}
+    </div>
+</template>
