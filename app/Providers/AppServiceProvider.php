c44acc40fff3541630d809300ef8955286837b5d
Lee Connelly <lee@leeconnelly.dev>
2024-04-20 15:49:45 +0000

    Disable lazy loading

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,5 +27,6 @@ class AppServiceProvider extends ServiceProvider
 
         JsonResource::withoutWrapping();
         Model::unguard();
+        Model::preventLazyLoading(! $this->app->isProduction());
     }
 }
