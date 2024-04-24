5ccafe5304e42bb3017c1ecf285ae40550521e69
Lee Connelly <lee@leeconnelly.dev>
2024-04-24 20:07:15 +0000

    Create navbar

---
+++ resources/js/Components/Navbar.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { Link } from '@inertiajs/vue3'
+import ApplicationLogo from '@/Components/ApplicationLogo.vue'
+import Moon from '@/Components/Moon.vue'
+</script>
+
+<template>
+  <aside class="h-[4.5rem] bg-[#373B53] md:h-20">
+    <nav class="grid grid-cols-[auto_1fr_auto_auto] items-center">
+      <Link href="/">
+        <ApplicationLogo class="h-[4.5rem] w-[4.5rem] md:h-20 md:w-20" />
+      </Link>
+      <button class="col-start-3 mr-6 md:mr-8" type="button">
+        <Moon class="" />
+      </button>
+      <div class="border-l border-l-blue-500 p-5 md:px-8 md:py-6">
+        <img
+          class="h-8 w-8 rounded-full"
+          src="/images/image-avatar.jpg"
+          alt="Avatar"
+          width="80"
+          height="80"
+        />
+      </div>
+    </nav>
+  </aside>
+</template>
--- resources/js/Pages/Invoices/Index.vue
+++ resources/js/Pages/Invoices/Index.vue
@@ -1,9 +1,17 @@
 <script setup>
+import Navbar from '@/Components/Navbar.vue'
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
+import { Head } from '@inertiajs/vue3'
+
 defineProps({
   invoices: Array,
 })
 </script>
 
 <template>
-  <div>invoices page</div>
+  <AuthenticatedLayout>
+    <Head title="Invoices" />
+
+    <div>invoices page</div>
+  </AuthenticatedLayout>
 </template>
